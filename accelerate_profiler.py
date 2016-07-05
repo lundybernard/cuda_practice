4a085469a71308e0df86aa970ff0f0c88286be51
Locky1138 <locky1138@gmail.com>
2016-07-05 18:24:51 -0500

    trying the accelerate profiler, failed to import

---
+++ accelerate_profiler.py
@@ -0,0 +1,16 @@
+from accelerate import profiler
+
+def dot(a, b):
+  sum=0
+  for i in range(len(a)):
+      sum += a[i]*b[i]
+  return sum
+
+a = np.arange(16, dtype=np.float32)
+b = np.arange(16, dtype=np.float32)
+
+p = profiler.Profile(signatures=False)
+p.enable()
+dot(a, b)
+p.disable()
+p.print_stats()
